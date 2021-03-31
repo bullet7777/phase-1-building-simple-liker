6efdad60e6a4df5335b76ffd3a006e12f277053f
chris <chris.mahfouz@hotmail.com>
2021-03-31 11:41:45 +0300

    completed assignment simple liker

--- main.js
+++ main.js
@@ -3,9 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.like-glyph').forEach(item => {
+    item.addEventListener('click',event => {
+
+          if(item.innerText===EMPTY_HEART){
+            mimicServerCall()
+            .then(() => {
+              item.classList.add('activated-heart')
+              item.innerText=FULL_HEART
+              
+            })
+            .catch((message) => {
+              let errorModal=document.getElementById('modal')
+              errorModal.classList.remove('hidden')
+              let modalMessage=document.getElementById('modal-message')
+              modalMessage.innerText=message
+              setTimeout(function(){ 
+                errorModal.classList.add("hidden")
+              }, 3000)
+              })
+ 
+          } else {
+            item.innerText=EMPTY_HEART
+            item.classList.remove('activated-heart')
+          }
+    })
+  })
 
 
 
+
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
